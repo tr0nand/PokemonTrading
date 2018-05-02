483ec8e5a3e6c5ea7f2e7772acb94b4f291688bb
123tarunanand <anandtarun2@gmail.com>
2018-05-02 17:53:22 +0530

    Basic blockchain done

---
+++ blockchain.py
@@ -0,0 +1,70 @@
+import hashlib
+import json
+from time import time
+
+
+
+#Blockchain class
+class Blockchain(object):
+    def _init_(self):
+        #Existing Blockchain
+        self.chain=[]
+        #Current trades pending
+        self.current_trade = []
+
+        #Create genesis block
+        self.new_block(proof=0,previous_hash='1')
+
+    def new_block(self,proof,previous_hash = None):
+        #Create a new block after mining in the blockchain
+        #previous_hash = None for the genesis block
+        block = {
+        'index':len(self.chain) + 1,
+        'timestamp': time(),
+        'trade': self.current_trade,
+        'proof': proof,
+        'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.current_trade=[]
+        self.chain.append(block)
+        return block
+
+
+
+    def new_transaction(self,trainer1,trainer2,sentby1,sentby2):
+        #creates a new trade to go into next mined block
+        self.current_trade.append(
+        {
+            'trainer1': trainer1,
+            'trainer2': trainer2,
+            'sentby1': sentby1,
+            'sentby2' : sentby2,
+        }
+        )
+        return self.last_block['index'] + 1
+
+    def proof_of_work(self,last_proof):
+        #Simple proof of work algorithm:
+
+        proof = 0;
+        while self.valid_proof(last_proof,proof) is False:
+            proof +=1
+
+        return proof
+
+    def valid_proof(last_proof,proof):
+        guess = (last_proof+proof).encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == '0000'
+
+    @staticmethod
+    def hash(block):
+        #Creates a SHA-256 hash of a block
+
+        block_string = json.dumps(block,sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain
+        pass
